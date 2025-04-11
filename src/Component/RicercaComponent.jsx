7173d34cb879973070a0f4ab759e3edffb6a2f42
Gabri0223 <gabrielecare0@gmail.com>
2025-04-11 18:26:53 +0200

    esame finito

--- src/Component/RicercaComponent.jsx
+++ src/Component/RicercaComponent.jsx
@@ -1,12 +1,26 @@
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
+import { useState } from "react";
 
 const RicercaComponent = function () {
+  const [city, setCity] = useState("");
+
   return (
     <div className="d-flex flex-row mb-4 d-md-none">
       <Form className="mx-5 d-flex">
-        <Form.Control type="text" placeholder="Cerca una località" />;
-        <Button>Invio</Button>
+        <Form.Control
+          type="text"
+          placeholder="Cerca una località"
+          onChange={(e) => setCity(e.target.value)}
+        />
+        ;
+        <Button
+          onClick={() => {
+            location.assign(`/dettagli/${city}`);
+          }}
+        >
+          Invio
+        </Button>
       </Form>
     </div>
   );
